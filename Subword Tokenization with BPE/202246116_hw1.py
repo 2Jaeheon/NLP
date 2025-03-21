566f8be604e64c913eb339b5b2ca92384b90c006
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-03-21 14:46:20 +0900

    ADD: Basic Architecture

--- Subword Tokenization with BPE/202246116_hw1.py
+++ Subword Tokenization with BPE/202246116_hw1.py
@@ -8,11 +8,49 @@
 # 6. 토큰화 알고리즘 구현
 # 7. 실행 제어 로직 구성
 
+import argparse  # 파이썬에서 명령행 옵션을 쉽게 파싱하기 위한 모듈
+
+
+# BPE 학습을 위한 클래스
 class BPETrainer:
-  def __init__(self):
-    print("BPETrainer")
+  def __init__(self, max_vocab_size: int):
+    self.max_vocab_size = max_vocab_size
+    self.vocab = {}
+    self.merge_rules = []
 
 
+# BPE 토크나이저 클래스
 class BPETokenizer:
-  def __init__(self):
-    print("BPETokenizer")
+  def __init__(self, merge_path: str):
+    self.merge_rules = self.load_merge_rules(merge_path)
+
+  def load_merge_rules(self, merge_path: str):
+    # 추후 구현 예정
+    return {}
+
+
+if __name__ == '__main__':
+  print("Hello, BPE!")
+  parser = argparse.ArgumentParser()
+  # 학습 관련 옵션
+  parser.add_argument('--train')
+  parser.add_argument('--max_vocab', type=int)
+  parser.add_argument('--vocab')
+  parser.add_argument('--merge')
+
+  # 추론 관련 옵션
+  parser.add_argument('--infer')
+  parser.add_argument('--input')
+  parser.add_argument('--output')
+
+  # 파싱
+  args = parser.parse_args()
+
+  # 학습 또는 추론에 따른 분기
+  if args.train:
+    trainer = BPETrainer(args.max_vocab)
+    # 추후 구현할 함수들
+
+  elif args.infer:
+    tokenizer = BPETokenizer(args.merge)
+    # 추후 구현할 함수들
